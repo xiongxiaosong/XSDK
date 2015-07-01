4ffa80a75eb0ec0988e7c5c0deacfe88b51082ad
xiongxs <583155684@163.com>
2015-07-01 21:42:23 +0800

    新增密码输入控件

---
+++ src/com/xxs/sdk/ui/gridpass/PasswordType.java
@@ -0,0 +1,10 @@
+package com.xxs.sdk.ui.gridpass;
+
+/**
+ * @author xiongxs
+ */
+public enum PasswordType {
+
+    NUMBER, TEXT, TEXTVISIBLE, TEXTWEB;
+
+}
---
+++ src/com/xxs/sdk/ui/gridpass/PasswordView.java
@@ -0,0 +1,23 @@
+package com.xxs.sdk.ui.gridpass;
+
+/**
+ * @author xiongxs
+ */
+interface PasswordView {
+
+    //void setError(String error);
+
+    String getPassWord();
+
+    void clearPassword();
+
+    void setPassword(String password);
+
+    void setPasswordVisibility(boolean visible);
+
+    void togglePasswordVisibility();
+
+    void setOnPasswordChangedListener(GridPasswordView.OnPasswordChangedListener listener);
+
+    void setPasswordType(PasswordType passwordType);
+}
